2a9bb9186db3f4c2009da5cc864bbe2e43f951fe
gionoa <gnoa001@fiu.edu>
2019-02-01 22:34:25 -0500

    Added vars

--- Matadoor/ViewController.swift
+++ Matadoor/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    double interestRate = 5;
+//    double taxesAverage = 4000;
+//    double insurance = 1200;
+//    double monthlyIncome =
+//    Double.parseDouble(tfMonthlyIncome.getText());
+//    System.out.println(monthlyIncome);
+//    double monthlyDebt =
+//    Double.parseDouble(tfTotalMonthlyDebt.getText());
+//    System.out.println(monthlyDebt);
+//    double downPayment =
+//    Double.parseDouble(tfDownpayment.getText());
+//    System.out.println(downPayment);
+//    int year = Integer.parseInt(tfTerm.getText());
+//    System.out.println(year);
+    
+    var interestRate = 5.0
+    var taxesAverage = 4000.0
+    var insurance = 1200.0
+    
+    var monthlyIncome: Double?
+    var monthlyDebt: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
